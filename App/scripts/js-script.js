ccb07cdd6f279be6a8cce8a31e902c2e066be311
vilaxxh <116578324+vilaxxh@users.noreply.github.com>
2022-10-24 19:46:28 +0300

    Add files via upload

---
+++ App/scripts/js-script.js
@@ -0,0 +1,11 @@
+function superClick(el) {
+    el.style.background = 'white';
+    el.style.color = 'black';
+}
+var h3Showed=false;
+
+function changeDisplay(){
+	const header3=document.getElementById("1");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed=!h3Showed;
+}
